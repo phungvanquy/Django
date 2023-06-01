c99450b818a2a3dd8d24e322d095f3ad953a8659
phungvanquy <phungvanquy97@gmail.com>
2023-06-01 17:43:52 +0300

    finish 14

--- Practice1/coolsite/women/views.py
+++ Practice1/coolsite/women/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 
+from .forms import AddPostForm
 from .models import *
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -27,7 +28,16 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse("Добавление статьи")
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            form.save()
+            return redirect('home')
+
+    else:
+        form = AddPostForm()
+    return render(request, 'women/addpage.html', {'form': form ,'menu':menu, 'title': "Добавление статьи"})
 
 
 def contact(request):
@@ -53,7 +63,8 @@ def show_post(request, post_slug):
     return render(request, 'women/post.html', context=context)
 
 
-def show_category(request, cat_id):
+def show_category(request
+                  , cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
     if len(posts) == 0:
         raise Http404()
