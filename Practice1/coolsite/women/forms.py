588d8aadf7575d6b9449368e955b64992d6e7e6d
Phung Van Quy <phungvanquy97@gmail.com>
2023-06-02 00:10:50 +0300

    finished 15

--- Practice1/coolsite/women/forms.py
+++ Practice1/coolsite/women/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from .models import *
 from django.core.exceptions import ValidationError
+
+from .models import *
 
 class AddPostForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
@@ -15,10 +16,9 @@ class AddPostForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
         }
 
-    
     def clean_title(self):
         title = self.cleaned_data['title']
-        if len(title) > 20:
+        if len(title) > 200:
             raise ValidationError('Длина превышает 200 символов')
 
         return title
